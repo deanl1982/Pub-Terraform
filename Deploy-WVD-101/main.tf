5c40dc3bb99967b0807b8185ed645e5ec9e257ca
TypicalLawrence <dal08082@gmail.com>
2020-11-03 17:23:41 +0000

    Initial-Version

---
+++ Deploy-WVD-101/main.tf
@@ -0,0 +1,72 @@
+# Get AzureRM Terraforn Provider
+provider "azurerm" {
+  version = "2.31.1" #Required for WVD
+  features {}
+}
+
+# Create Resource Group - This will host all subsequent deployed resources
+resource "azurerm_resource_group" "default" {
+  name     = var.rgname
+  location = var.region
+}
+
+# Create "Pooled" WVD Host Pool
+resource "azurerm_virtual_desktop_host_pool" "pooleddepthfirst" {
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  name                     = var.pooledhpname
+  friendly_name            = var.pooledhpfriendlyname
+  description              = var.pooledhpdescription
+  type                     = "Pooled"
+  maximum_sessions_allowed = 50
+  load_balancer_type       = "DepthFirst"
+}
+
+#Create RemoteApp Application Group
+resource "azurerm_virtual_desktop_application_group" "pooledremoteapp" {
+  name                = var.pooledhpremoteappname
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  type          = "RemoteApp"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.pooleddepthfirst.id
+  friendly_name = var.pooledhpremoteappfriendlyname
+  description   = var.pooledhpremoteappdescription
+}
+
+#Create Desktop Application Group
+resource "azurerm_virtual_desktop_application_group" "pooleddesktopapp" {
+  name                = var.pooledhpdesktopappname
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  type          = "Desktop"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.pooleddepthfirst.id
+  friendly_name = var.pooledhpdesktopappfriendlyname
+  description   = var.pooledhpdesktopappdescription
+}
+
+# Create Workspace
+resource "azurerm_virtual_desktop_workspace" "workspace" {
+  name                = var.workspace
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  friendly_name = var.workspacefriendlyname
+  description   = var.workspacedesc
+}
+
+# Associate RemoteApp Application Group with Workspace
+resource "azurerm_virtual_desktop_workspace_application_group_association" "workspaceremoteapp" {
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+  application_group_id = azurerm_virtual_desktop_application_group.pooledremoteapp.id
+
+}
+
+# Associate Desktop Application Group with Workspace
+resource "azurerm_virtual_desktop_workspace_application_group_association" "workspacedesktop" {
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+  application_group_id = azurerm_virtual_desktop_application_group.pooleddesktopapp.id
+
+}
---
+++ Deploy-WVD-101/variables.tf
@@ -0,0 +1,69 @@
+variable "rgname" {
+  description = "Resource Group Name"
+  default     = "VFF-WUS-RG-TFWVD"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "West US 2"
+}
+
+variable "pooledhpname" {
+  description = "Pooled Host Pool Name"
+  default     = "VFF-WUS-TF-Pooled"
+}
+
+variable "pooledhpfriendlyname" {
+  description = "Pooled Host Pool Friendly Name"
+  default     = "VFF Pooled Host Pool"
+}
+
+variable "pooledhpdescription" {
+  description = "Pooled Host Pool Description"
+  default     = "VFF Pooled Host Pool"
+}
+
+variable "pooledhpremoteappname" {
+  description = "Pooled Host Pool RemoteApp App Group Name"
+  default     = "VFF-WUS-TF-Pooled-RA"
+}
+
+variable "pooledhpremoteappfriendlyname" {
+  description = "Pooled Host Pool RemoteApp App Group Friendly Name"
+  default     = "VFF Pooled Host Pool Remote Apps"
+}
+
+variable "pooledhpremoteappdescription" {
+  description = "Pooled Host Pool RemoteApp App Group Description"
+  default     = "VFF Pooled Host Pool Remote Apps"
+}
+
+variable "pooledhpdesktopappname" {
+  description = "Pooled Host Pool Desktop App Group Friendly Name"
+  default     = "VFF-WUS-TF-Pooled-DT"
+}
+
+variable "pooledhpdesktopappfriendlyname" {
+  description = "Pooled Host Pool Desktop App Group Friendly Name"
+  default     = "VFF Pooled Host Pool Remote Apps"
+}
+
+variable "pooledhpdesktopappdescription" {
+  description = "Pooled Host Pool Desktop App Group Description"
+  default     = "VFF Pooled Host Pool Remote Apps"
+}
+
+variable "workspace" {
+  description = "WVD Workspace Name"
+  default     = "VFF-Terraform-Workspace"
+}
+
+variable "workspacefriendlyname" {
+  description = "WVD Workspace Friendly Name"
+  default     = "VFF-Terraform-Workspace"
+}
+
+variable "workspacedesc" {
+  description = "WVD Workspace Description"
+  default     = "VFF-Terraform-Workspace"
+}
