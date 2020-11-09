d8f976d2e3471c25f6a5a2e0f1933fbeb0e227b0
TypicalLawrence <dal08082@gmail.com>
2020-11-09 20:26:41 +0000

    Updated variables to show deployed from module

--- Deploy-WVD-from-Module/main.tf
+++ Deploy-WVD-from-Module/main.tf
@@ -15,18 +15,18 @@ terraform {
 
 module "WVD-as-a-Module" {
   source                         = "../Modules/WVD-as-a-Module"
-  rgname                         = "VFF-USE-RG-WVD-REMOTE"
+  rgname                         = "VFF-USE-RG-WVD-Module"
   region                         = "West US 2"
-  pooledhpname                   = "VFF-WUS-TFRM-Pooled"
+  pooledhpname                   = "VFF-WUS-TFRM-Module"
   pooledhpfriendlyname           = "VFF Pooled Host Pool"
   pooledhpdescription            = "VFF Pooled Host Pool"
-  pooledhpremoteappname          = "VFF-WUS-TFRM-Pooled-RA"
+  pooledhpremoteappname          = "VFF-WUS-TFRM-Module-RA"
   pooledhpremoteappfriendlyname  = "VFF Pooled Host Pool Remote Apps"
   ooledhpremoteappdescription    = "VFF Pooled Host Pool Remote Apps"
-  pooledhpdesktopappname         = "VFF-WUS-TFRM-Pooled-DT"
+  pooledhpdesktopappname         = "VFF-WUS-TFRM-Module-DT"
   pooledhpdesktopappfriendlyname = "VFF Pooled Host Pool Remote Apps"
   pooledhpdesktopappdescription  = "VFF Pooled Host Pool Remote Apps"
-  workspace                      = "VFF-Terraform-Workspace"
+  workspace                      = "VFF-Terraform-Workspace-Module"
   workspacefriendlyname          = "VFF-Terraform-Workspace"
   workspacedesc                  = "VFF-Terraform-Workspace"
   pooledhpmaxsessions            = 50
